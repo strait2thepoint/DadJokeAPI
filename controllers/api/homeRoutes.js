7ce890cc930a690bfa51794f9d19f14ed09ccb9d
Andrew Gomoll <andrewgomoll1@gmail.com>
2023-05-11 21:05:32 -0400

    Added routes to index and homeRoutes files in the controllers file

---
+++ controllers/api/homeRoutes.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const User = require('../../models/User');
+
+app.get('././models/User', (req, res) => {
+
+    res.send('Get all Users');
+  });
+
+app.get('././models/Joke', (req, res) => {
+
+     res.send('Get all Jokes');
+  });
+
+//need to change port from 3000
+app.listen(3000, () => {
+    console.log('Server started on port 3000');
+  });
