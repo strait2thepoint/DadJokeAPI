71dd1ec4677c60f3f2f946dfe62270aa10ad8bd8
lawankar.anuja08@gmail.com <lawankar.anuja08@gmail.com>
2023-05-14 16:48:05 -0400

    Added code for model in database for user and joke

--- models/Joke.js
+++ models/Joke.js
@@ -1,35 +1,43 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection'); //fix these links??
-const User = require('./User');
+//const User = require('./User');
 
-class Joke extends Model {}
+class Joke extends Model { }
 
 Joke.init(
-    {
-      jokeId: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-      },
-      jokeSetUp: {
-        type: DataTypes.STRING(120),
-        allowNull: false,
-      },
-      jokePunchLine: {
-        type: DataTypes.STRING(120),
-        allowNull: false,
-      }, 
-      userId: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        references:{
-            model: User,
-            key: 'id'
-        },
+  {
+    jokeId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    jokeSetUp: {
+      type: DataTypes.STRING(120),
+      allowNull: false,
+    },
+    jokePunchLine: {
+      type: DataTypes.STRING(120),
+      allowNull: false,
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'user',
+        key: 'id'
       },
     },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'joke',
+  }
 ),
-Joke.belongsTo(User, { foreignKey: 'userId' });
 
-module.exports = Joke;
+  //Joke.belongsTo(User, { foreignKey: 'userId' });
+
+  module.exports = Joke;
