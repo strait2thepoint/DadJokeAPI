3a49ca86907199401a028b695c6d8a4e818f5969
Andrew Gomoll <andrewgomoll1@gmail.com>
2023-05-14 15:36:06 -0400

    fixed errors in routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 
-const userRoutes = require('./userRoutes');
+const homeRoutes = require('./homeRoutes');
 
-router.use('././models/User', userRoutes);
+router.use('/homeRoutes', homeRoutes);
+
 
 module.exports = router;
