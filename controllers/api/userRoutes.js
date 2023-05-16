2e85622cb4e844e413fbd1a893c4d3dcebffb77f
Dennis-Inglehart <Haikon@twosixfour.org>
2023-05-15 22:13:32 -0400

    add the changes that Mark walked Dennis through

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -14,15 +14,14 @@ router.get('/', (req, res) => {
   });
 
   router.get('/:id', (req, res) => {
+    const { id, name, email, password } = rec.params // <-- this is the 'object destructuring thing Mark mentioned
     User.findOne({
       attributes: { exclude: ['password'] },
-      where: {
-        id: req.params.id
-      },
+      where: {id},
       include: [
         {
-          model: Post,
-          attributes: [user_id, userData] //TODO: add all attributes
+          model: User,
+          attributes: [id, name, email, password] //TODO: add all attributes
         },
         
       ]
