250db40b0d01557157dd828f5a64507003f1fa71
lawankar.anuja08@gmail.com <lawankar.anuja08@gmail.com>
2023-05-18 21:00:12 -0400

    Add files like add-joke, delete-joke, edit-joke in js

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,26 +1,26 @@
-//rename this page topFive???
 const newFormHandler = async (event) => {
   event.preventDefault();
 
   const name = document.querySelector('#project-name').value.trim();
+  const needed_funding = document.querySelector('#project-funding').value.trim();
   const description = document.querySelector('#project-desc').value.trim();
 
-  // if (name && description) {
-  //   const response = await fetch(`/api/projects`, {
-  //     method: 'POST',
-  //     body: JSON.stringify({ name, needed_funding, description }),
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //     },
-  //   });
+  if (name && needed_funding && description) {
+    const response = await fetch(`/api/projects`, {
+      method: 'POST',
+      body: JSON.stringify({ name, needed_funding, description }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
 
     if (response.ok) {
-      document.location.replace('/profile');//rename topFive?
+      document.location.replace('/profile');
     } else {
       alert('Failed to create project');
     }
   }
-// };
+};
 
 const delButtonHandler = async (event) => {
   if (event.target.hasAttribute('data-id')) {
