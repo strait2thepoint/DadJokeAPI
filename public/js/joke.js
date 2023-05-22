f8fac223a9c832d939ef8d384bcd7323e6506a67
Strait2thePoint <122751055+strait2thepoint@users.noreply.github.com>
2023-05-22 18:28:36 -0400

    easteregg

--- public/js/joke.js
+++ public/js/joke.js
@@ -1,7 +1,21 @@
-const newFormHandler = async (event) => {
-    event.preventDefault();
- 
-    
-    
-}
 
+// Step 1: Retrieve the div element
+const myDiv = document.getElementById('rickroll');
+
+// Step 2: Attach an event listener
+myDiv.addEventListener('click', function() {
+  // Create a video element
+  const iframe = document.createElement('iframe');
+  
+  // Set attributes and source for the iframe
+  iframe.width = '560';
+  iframe.height = '315';
+  iframe.src = 'https://www.youtube.com/embed/dQw4w9WgXcQ';
+  iframe.title = 'YouTube video player';
+  iframe.frameBorder = '0';
+  iframe.allow = 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share';
+  iframe.allowFullscreen = true;
+  
+  // Append the iframe element to the div
+  myDiv.appendChild(iframe);
+});
